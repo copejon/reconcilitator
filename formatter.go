046dd89ecd5c01159a9a1aae4f222903c6b5a74c
jcope <jcope@redhat.com>
2020-01-01 14:50:37 -0600

    cleaning up code before Summer does something irreparable
    Signed-off-by: jcope <jcope@redhat.com>

--- formatter.go
+++ formatter.go
@@ -7,17 +7,6 @@ import (
 	"time"
 )
 
-type tableFormatter struct {
-	w   table.Writer
-	reg register.Register
-}
-
-type tableFormatterOpts struct {
-	since     time.Time
-	numDays   int
-	autoIndex bool
-}
-
 const (
 	date = iota + 1 // row count begins at 1
 	_               // payee
@@ -26,19 +15,32 @@ const (
 	sort
 )
 
-var columnHeaders = table.Row{"Date", "Payee", "Amount", "Cleared", "Sort"}
+func columnHeaders() table.Row {
+	return table.Row{"Date", "Payee", "Amount", "Cleared", "Sort"}
+}
 
 const layout = "01/02/2006"
 
-var columnConfig = []table.ColumnConfig{
-	{
-		Number:      date,
-		Transformer: text.NewTimeTransformer(layout, nil),
-	},
-	{
-		Number:      amount,
-		Transformer: text.NewNumberTransformer("%.2f"),
-	},
+const currencyFormat = "%.2f"
+
+func columnConfig() []table.ColumnConfig {
+	return []table.ColumnConfig{
+		{
+			Number:      date,
+			Transformer: text.NewTimeTransformer(layout, nil),
+		},
+		{
+			Number:      amount,
+			Transformer: text.NewNumberTransformer(currencyFormat),
+		},
+	}
+}
+
+func sortConfig() []table.SortBy {
+	return []table.SortBy{{
+		Number: sort,
+		Mode:   table.DscNumeric,
+	}}
 }
 
 func paintUncleared(row table.Row) text.Colors {
@@ -48,25 +50,46 @@ func paintUncleared(row table.Row) text.Colors {
 	return nil
 }
 
-func NewTableFormatter(r register.Register, title string, opts *tableFormatterOpts) *tableFormatter {
-	tw := table.NewWriter()
-	tw.SetAutoIndex(opts.autoIndex)
-	tw.SetTitle(title)
-	tw.AppendHeader(columnHeaders)
+type tableFormatter struct {
+	w     table.Writer
+	title string
+	opts  *tableFormatterOpts
+	reg   register.Register
+}
 
-	tw.SortBy([]table.SortBy{{
-		Number: sort,
-		Mode:   table.DscNumeric,
-	}})
-	tw.SetRowPainter(paintUncleared)
-	tw.SetColumnConfigs(columnConfig)
+type tableFormatterOpts struct {
+	since     time.Time
+	numDays   int
+	autoIndex bool
+}
 
-	for _, day := range r.Entries() {
+func defaultOpts() *tableFormatterOpts {
+	return &tableFormatterOpts{
+		since:     time.Time{},
+		numDays:   -1,
+		autoIndex: true,
+	}
+}
+
+func (t *tableFormatter) setWriter() (tw table.Writer) {
+	t.w = table.NewWriter()
+	t.w.SetTitle(t.title)
+	t.w.SortBy(sortConfig())
+	t.w.SetAutoIndex(t.opts.autoIndex)
+	t.w.SetRowPainter(paintUncleared)
+	t.w.AppendHeader(columnHeaders())
+	t.w.SetColumnConfigs(columnConfig())
+	return
+}
+
+func (t *tableFormatter) loadTable() {
+	opts := t.opts
+	for _, day := range t.reg.Entries() {
 		for _, e := range day {
 			// if the entry falls on or after opts.since, append it to the table
 			if e.Date().After(opts.since) ||
 				e.Date().Equal(opts.since) {
-				tw.AppendRow(table.Row{
+				t.w.AppendRow(table.Row{
 					e.Date(),
 					e.Payee(),
 					e.Amount(),
@@ -76,18 +99,17 @@ func NewTableFormatter(r register.Register, title string, opts *tableFormatterOp
 			}
 		}
 	}
-	return &tableFormatter{
-		w:   tw,
-		reg: r,
-	}
 }
 
-func defaultOpts() *tableFormatterOpts {
-	return &tableFormatterOpts{
-		since:     time.Time{},
-		numDays:   -1,
-		autoIndex: true,
+func NewTableFormatter(r register.Register, title string, opts *tableFormatterOpts) *tableFormatter {
+	tf := &tableFormatter{
+		title: title,
+		opts:  opts,
+		reg:   r,
 	}
+	tf.w = tf.setWriter()
+	tf.loadTable()
+	return tf
 }
 
 func NewDefaultTableFormatter(r register.Register, title string) *tableFormatter {
